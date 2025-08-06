b698f5512fed4335d3052599f1b33c4ed79ac77a
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-08-06 11:55:48 +0200

    bugfix hopefully?

--- Source/Source/Hediff_PainStrength.cs
+++ Source/Source/Hediff_PainStrength.cs
@@ -6,14 +6,13 @@ namespace RMLNDS_CanonishXenotypes
     public class Hediff_PainStrength : Hediff
     {
         public bool IsLethal => false;
-        public float initialSeverity => 0.0f;
         public bool EverCurableByItem => false;
         public bool causesNoPain => true;
         public override float Severity
         {
             set
             {
-                this.Severity = (this.pawn.health.hediffSet.PainTotal);
+                this.Severity = (this.pawn.health.hediffSet.PainTotal + 1);
             }
             get => this.Severity;
         }
