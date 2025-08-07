25f2bd79c3441e7ccc2be3113961edc1b2d4f855
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-08-07 18:09:27 +0200

    Update Hediff_PainStrength.cs

--- Source/Source/Hediff_PainStrength.cs
+++ Source/Source/Hediff_PainStrength.cs
@@ -6,7 +6,6 @@ namespace RMLNDS_CanonishXenotypes
     public class Hediff_PainStrength : Hediff
     {
         public bool IsLethal => false;
-        public bool EverCurableByItem => false;
         public bool causesNoPain => true;
         public override float Severity => (this.pawn.health.hediffSet.PainTotal + 1);
     }
